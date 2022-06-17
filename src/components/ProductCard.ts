72e76e5bae62dd4fb572cb0c1e5ed9c790a6b889
龙思宇 <longsiyu@xd.com>
2022-06-17 11:49:00 +0800

    Add mobile styles

--- src/components/ProductCard.ts
+++ src/components/ProductCard.ts
@@ -5,16 +5,16 @@ export default defineComponent({
     product: Object,
   },
   render() {
-    return h(
+    return h("div", { class: "product-card" }, h(
       "a",
-      { href: `/products/${this.product?.id}`, class: "product-card" },
+      { href: `/products/${this.product?.id}` },
       [
-        h("img", { src: this.product?.images?.[0] }),
+        h("div", { class: "square" }, h("img", { src: this.product?.images?.[0] })),
         h("div", { class: "card-text" }, [
           h("div", { class: "name" }, this.product?.name),
           h("div", { class: "price" }, this.product?.price)
         ]),
       ]
-    );
+    ));
   },
 });
