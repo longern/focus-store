97c8f820bdef8223487a846d210f5378328c4f99
longern <771815005@qq.com>
2022-06-19 00:37:35 +0800

    Add cover image

--- src/pages/admin/Website.vue
+++ src/pages/admin/Website.vue
@@ -18,26 +18,47 @@ onBeforeMount(() => {
   <div>
     <h1 v-text="t('Website')"></h1>
     <form>
-      <div class="form-group">
-        <label for="site-title">
-          <span v-text="t('Title')"></span>
-          <input
-            type="text"
-            class="form-control"
-            id="site-title"
-            v-model="site.title"
-          />
-        </label>
-      </div>
+      <label>
+        <span v-text="t('Title')"></span>
+        <input type="text" v-model="site.title" />
+      </label>
+      <label>
+        <span v-text="t('Cover Image')"></span>
+        <input type="file" />
+      </label>
     </form>
   </div>
 </template>
 
+<style>
+label {
+  display: block;
+  min-height: 32px;
+  padding: 12px 0;
+}
+
+label > span {
+  display: inline-block;
+  width: 192px;
+}
+
+input[type="text"],
+input[type="number"],
+input[type="email"] {
+  font-size: 1em;
+  padding: 6px;
+  border: 1px solid darkgray;
+  border-radius: 4px;
+}
+</style>
+
 <i18n lang="yaml">
 en:
+  Cover Image: Cover Image
   Title: Title
   Website: Website
 zh-CN:
+  Cover Image: 封面图片
   Title: 标题
   Website: 网站
 </i18n>
