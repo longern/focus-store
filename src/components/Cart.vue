9ac8399c2751a66d5b7651f737059641c8319acd
longern <771815005@qq.com>
2022-06-21 09:10:42 +0800

    Add cart page

---
+++ src/components/Cart.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { mdiClose } from "@mdi/js";
+import SvgIcon from "@jamescoyle/vue-icon";
+
+import { cart } from "@/composables/states";
+
+function removeFromCart(product) {
+  cart.value.splice(cart.value.indexOf(product), 1);
+}
+</script>
+
+<template>
+  <main>
+    <div class="cart-items">
+      <div class="cart-item" v-for="item in cart" :key="item.id">
+        <div class="cart-item-image">
+          <img :src="item.images?.[0]" />
+        </div>
+        <div class="cart-item-details">
+          <div class="cart-item-name">{{ item.name }}</div>
+          <div class="cart-item-price">{{ item.price }}</div>
+        </div>
+        <button class="btn-icon" @click="removeFromCart(item)">
+          <SvgIcon type="mdi" :path="mdiClose"></SvgIcon>
+        </button>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style>
+.cart-item {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
+</style>
