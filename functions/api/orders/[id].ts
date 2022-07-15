88403c8b1f431ed3955639600199f1f5058df555
longern <771815005@qq.com>
2022-07-15 09:15:03 +0800

    Add get order API

---
+++ functions/api/orders/[id].ts
@@ -0,0 +1,9 @@
+export async function onRequestGet({ params, env }) {
+  const value = await env.NAMESPACE.get(`orders:${params.id}`);
+  if (value === null) {
+    return new Response("Not found", { status: 404 });
+  }
+  return new Response(value, {
+    headers: { "Content-Type": "application/json" },
+  });
+}
